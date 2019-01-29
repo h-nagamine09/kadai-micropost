25331c7bf4892f8ee58e12b648b12c9ad91ea6a5
lifestream <hiroki.trance.promo@gmail.com>
2019-01-29 06:58:08 +0000

    User Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  include SessionsHelper
+  
+  #users#index,users#showのアクションには必ず事前にログイン認証を確認する処理
+  private
+  
+   def require_user_logged_in
+     #unlessはifの反対。falseの時に処理を実行
+     unless logged_in?
+      redirect_to login_url
+     end
+   end 
 end
+
+#ApplicationControllerにメソッドを定義すると全てのControllerで定義したメソッドが使用できる
