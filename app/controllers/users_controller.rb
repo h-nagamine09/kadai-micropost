ee60f04aedc3daf8c1949b78b78d1e9ff00d8582
lifestream <hiroki.trance.promo@gmail.com>
2019-02-12 09:08:58 +0000

    mylikesボタンをnavbarに表示に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,7 @@ class UsersController < ApplicationController
   #Strong Paramter セキュリティのため
   #name,email,password,password_confirmationを許可。
   private
+  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end 
